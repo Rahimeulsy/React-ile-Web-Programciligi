9a0b3a4ae8315367194a4212cb867afaf9e1a40f
RahimeUlusoy <88655508+Rahimeulsy@users.noreply.github.com>
2024-08-25 17:13:08 -0700

    getElementsByName

--- dom1.js
+++ dom1.js
@@ -1,9 +1,18 @@
-var intro1 = document.getElementById("intro1");
-var mesaj = document.getElementById("mesaj");
-mesaj.innerHTML = intro1.innerHTML;
-var tumListeler = document.getElementsByTagName("ul");
-var sehirler = tumListeler[0];
-tumElemanlar = sehirler.getElementsByTagName("li");
-for (i = 0; i < tumElemanlar.length; i++) {
-    alert(tumElemanlar[i].innerHTML);
-}
+// var intro1 = document.getElementById("intro1");
+// var mesaj = document.getElementById("mesaj");
+// mesaj.innerHTML = intro1.innerHTML;
+// var tumListeler = document.getElementsByTagName("ul");
+// var sehirler = tumListeler[0];
+// tumElemanlar = sehirler.getElementsByTagName("li");
+// for (i = 0; i < tumElemanlar.length; i++) {
+//     alert(tumElemanlar[i].innerHTML);
+// }
+
+// var classElemanlari = document.getElementsByClassName("intro1");
+// alert(classElemanlari[1].innerHTML);
+
+// var queryElemanları =document.querySelectorAll("p.intro1");
+// alert(queryElemanları.length);
+
+var isimElemanlari =document.getElementsByName("musteriAdi");
+alert(isimElemanlari[0].value);
