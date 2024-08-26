037caa07d1345acf982d75f61a3d50f4f41939fe
RahimeUlusoy <88655508+Rahimeulsy@users.noreply.github.com>
2024-08-25 18:18:04 -0700

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,32 @@
+var sayi = 10;
+var sehir = "Ankara";
+
+sayi = 11;
+sehir = "İstanbul";
+var sehir = "Adana";
+
+let il = "Ankara";
+il = "Bursa";
+//let il = "izmir";
+
+function mesajVer() {
+    var isim="Engin";
+    console.log(sehir);
+    console.log(isim);
+}
+// console.log(sayi);
+// console.log(sehir);
+// console.log(il);
+
+// mesajVer();
+// console.log(isim);
+
+// for(let i=1; i<10; i++){
+// }
+// console.log(i);
+
+const soyad= "Demiroğ"
+const sehirler =["Ankara","İstanbul"]
+sehirler.push("İzmir");
+sehirler=["Bursa","Mardin"]
+console.log(sehirler)
